d44985d93970ca476df157ec7c38c5160c15173d
maruju11 <maruju@gmail.com>
2018-11-02 22:32:35 -0300

    functins2

--- 10_functions/01/main.go
+++ 10_functions/01/main.go
@@ -1,12 +1,18 @@
 package main
 
 import "fmt"
+impr(
+	L "./media"
+)
 
 func main() {
 	fmt.Println("indice de massa corporal")
 	z := indPeso(69.0, 1.77)
 	fmt.Println(z)
 	fmt.Println(indPeso(75, 1.77))
+	fmt.Println("--------------------------------------------------------------------------")
+	z2 := []float64{100, 0200, 150, 300, 220, 310, 222}
+	fmt.Println(L.Media(z2))
 }
 
 func indPeso(x, y float64) float64 {
--- 10_functions/01/media.go
+++ 10_functions/01/media.go
@@ -1,8 +1,11 @@
-package  media
+package 01
+
 import "fmt"
 
-func media(xs []float64)float64{
-	total:=0.0
-	for _, soma:=range
-	return
+func Media(xs []float64) float64 {
+	total := 0.0
+	for _, soma := range xs {
+		total += soma
+	}
+	fmt.Println(total / float64(len(xs)))
 }

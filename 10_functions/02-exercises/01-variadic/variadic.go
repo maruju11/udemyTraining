a2d8b906a654fa90ecf78dc0512aafd34e5269b4
maruju11 <maruju@gmail.com>
2018-11-16 11:25:59 -0200

    variadic

--- 10_functions/02-exercises/01-variadic/variadic.go
+++ 10_functions/02-exercises/01-variadic/variadic.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 )
 
-func maior(args ...int) int {
-	tot := 0
+func main() {
+	data := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	n := maior(data...)
+	m := media(data...)
+	fmt.Println(n, m)
+}
+func maior(args ...float64) float64 {
+	tot := 0.0
 	for _, v := range args {
 		if tot <= v {
 			tot = v
 		}
-
 	}
 	return tot
 }
+func media(args1 ...float64) float64 {
+	tot1 := 0.0
+	for _, v1 := range args1 {
+		tot1 += v1
+	}
+	fmt.Println(args1)
+	fmt.Println(len(args1))
+	fmt.Println(tot1)
+	return tot1 / float64(len(args1))
 
-func main() {
-	fmt.Println(maior(1, 2, 22, 3, 4, 5, 6, 7))
 }

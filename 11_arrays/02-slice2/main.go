1ab404ae5249baa0af8993701bbe07815d73bb61
maruju11 <maruju@gmail.com>
2018-11-18 18:26:02 -0200

    slicepointer

--- 11_arrays/02-slice2/main.go
+++ 11_arrays/02-slice2/main.go
@@ -4,15 +4,19 @@ import "fmt"
 
 func main() {
 	slice := []int{1, 2, 3, 4, 5}
-	newSlice := slice[0:2:5]
+	newSlice := slice[0:3:3]
 	fmt.Println(newSlice)
 	newSlice = append(newSlice, 6)
 	newSlice = append(newSlice, 7)
 	fmt.Println(newSlice)
-	newSlice = append(newSlice, 8, 9, 10, 11, 12, 13)
-	slice = append(slice, 222, 231, 21, 22, 23, 24)
+	newSlice = append(newSlice, 8, 9, 10)
+	slice = append(slice, 21, 22, 23, 24)
 	fmt.Println(slice, newSlice)
 	//fmt.Printf("%T \n", slice)
 	fmt.Printf("%T \n", newSlice)
 	fmt.Println(len(slice), len(newSlice))
+	fmt.Printf("%p\n", append(newSlice, slice...))
+	for index, value := range slice {
+		fmt.Printf("iNDEX = %d   VAlour: %d value-address= %X Elem-address = %X\n", index, value, &value, &slice[index])
+	}
 }

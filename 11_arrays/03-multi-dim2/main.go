29960f0f972a03051ca0acfdbbc2aca68ebffc79
maruju11 <maruju@gmail.com>
2018-11-24 22:07:21 -0200

    slices

--- 11_arrays/03-multi-dim2/main.go
+++ 11_arrays/03-multi-dim2/main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
-	records := make
+	records := make([][]string, 0)
+	//student1
+	student1 := make([]string, 4)
+	student1[0] = "Jorge"
+	student1[1] = "Saito"
+	student1[2] = "100.00"
+	student1[3] = "74.00"
+	//store the records
+	records = append(records, student1)
+	//student2
+	student2 := make([]string, 4)
+	student2[0] = "Sandra"
+	student2[1] = "Willy Lo"
+	student2[2] = "99.00"
+	student2[3] = "92.00"
+	records = append(records, student2)
+	fmt.Println(records)
+
+	transactions := make([][]int, 0, 3)
+	for i := 0; i < 3; i++ {
+		transaction := make([]int, 0)
+		for j := 0; j < 4; j++ {
+			transaction = append(transaction, j)
+		}
+		transactions = append(transactions, transaction)
+		fmt.Println(transactions)
+	}
+
 }
